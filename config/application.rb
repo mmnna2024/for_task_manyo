4a3265c765712aff5128dc7c927c202eb8ef3ce7
naomu <muramatsu2023@gmail.com>
2024-02-22 18:02:17 +0900

    タイムゾーン表記設定

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module CdpWebManyoTask
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
   end
 end

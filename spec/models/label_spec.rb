6dc2e5c2c16cf01ec333be3d9db5e8923832101d
naomu <muramatsu2023@gmail.com>
2024-03-06 14:14:52 +0900

    add:label model spec

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'ラベルモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    let!(:user) { FactoryBot.create(:user, id: 1) }
+    let!(:label) { FactoryBot.create(:label, user_id: 1) }
+
+    context 'ラベルの名前が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        label.update(name: nil)
+        expect(label).to be_invalid
+        expect(label.errors.full_messages).to eq ["名前を入力してください"]
+      end
+    end
+
+    context 'ラベルの名前に値があった場合' do
+      it 'バリデーションに成功する' do
+        expect(label).to be_valid
+      end
+    end
+  end
 end
---
+++ spec/system/labels_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "ラベル管理機能", type: :system do
+  describe "登録機能" do
+    context "ラベルを登録した場合" do
+      it "登録したラベルが表示される" do
+      end
+    end
+  end
+  describe "一覧表示機能" do
+    context "一覧画面に遷移した場合" do
+      it "登録済みのラベル一覧が表示される" do
+      end
+    end
+  end
+end
